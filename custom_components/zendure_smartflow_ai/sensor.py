1d566975a8f721b3868007b8ecffdbc65be90009
Thomas Kuhn <84145136+PalmManiac@users.noreply.github.com>
2026-02-08 11:40:35 +0100

    Update sensor.py

--- custom_components/zendure_smartflow_ai/sensor.py
+++ custom_components/zendure_smartflow_ai/sensor.py
@@ -287,23 +287,6 @@ class ZendureSmartFlowSensor(SensorEntity):
         data = self.coordinator.data or {}
         return data.get("details", {})
 
-        if self.entity_description.key in (
-            "status",
-            "ai_status",
-            "recommendation",
-            "decision_reason",
-            "ai_debug",
-            "planning_status",
-            "planning_active",
-            "planning_target_soc",
-            "planning_reason",
-            "next_action_state",
-            "next_planned_action",
-        ):
-            return details
-
-        return None
-
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(
             self.coordinator.async_add_listener(self.async_write_ha_state)
