3119e8d83f6eec8e3604f1e389573265743165bd
Thomas Kuhn <84145136+PalmManiac@users.noreply.github.com>
2026-02-15 16:07:44 +0100

    Update __init__.py

--- custom_components/battery_smartflow_ai/__init__.py
+++ custom_components/battery_smartflow_ai/__init__.py
@@ -3,51 +3,20 @@ from __future__ import annotations
 import logging
 
 import homeassistant.helpers.config_validation as cv
-
-CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, PLATFORMS
 from .coordinator import ZendureSmartFlowCoordinator
 
-_LOGGER = logging.getLogger(__name__)
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
-OLD_DOMAIN = "zendure_smartflow_ai"
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Handle migration from old domain."""
-    old_entries = hass.config_entries.async_entries(OLD_DOMAIN)
-
-    if old_entries:
-        _LOGGER.info("Migrating Zendure SmartFlow AI entries to Battery SmartFlow AI")
-
-    for old_entry in old_entries:
-        hass.async_create_task(_migrate_entry(hass, old_entry))
-
+    """Set up the integration (YAML not supported)."""
     return True
-
-
-async def _migrate_entry(hass: HomeAssistant, old_entry: ConfigEntry) -> None:
-    """Migrate old config entry to new domain."""
-    _LOGGER.info("Migrating config entry %s to new domain %s", old_entry.entry_id, DOMAIN)
-
-    data = dict(old_entry.data)
-    options = dict(old_entry.options)
-
-    new_entry = hass.config_entries.async_create_entry(
-        title=old_entry.title,
-        data=data,
-        options=options,
-        domain=DOMAIN,
-    )
-
-    await hass.config_entries.async_remove(old_entry.entry_id)
-
-    if new_entry:
-        await hass.config_entries.async_reload(new_entry.entry_id)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
