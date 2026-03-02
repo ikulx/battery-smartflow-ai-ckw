f70ec3cd5bf7b1f42e066bfce2af1e5c29a4d1f0
Thomas Kuhn <84145136+PalmManiac@users.noreply.github.com>
2026-03-02 19:44:42 +0100

    Update const.py

--- custom_components/battery_smartflow_ai/const.py
+++ custom_components/battery_smartflow_ai/const.py
@@ -10,7 +10,7 @@ DOMAIN = "battery_smartflow_ai"
 INTEGRATION_NAME = "Battery SmartFlow AI"
 INTEGRATION_MANUFACTURER = "PalmManiac"
 INTEGRATION_MODEL = "Home Assistant Integration"
-INTEGRATION_VERSION = "2.0.6"
+INTEGRATION_VERSION = "3.0.0-Beta1"
 
 PLATFORMS: list[Platform] = [
     Platform.SENSOR,
